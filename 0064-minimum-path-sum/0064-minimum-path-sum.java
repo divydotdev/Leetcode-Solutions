a4a47e72fe0d03e81b94e58669289e434fe89e6b
Divy Dev Jaiswal <182238614+divydotdev@users.noreply.github.com>
2026-09-06 12:28:23 +0530

    Time: 1 ms (99.82%), Space: 50.2 MB (72.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,8 +1,9 @@
+/*
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
+
         for (int i = 1; i < m; i++) {
             grid[i][0] += grid[i-1][0];
         }
@@ -19,4 +20,27 @@ class Solution {
         
         return grid[m-1][n-1];
     }
+}
+*/
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return fk(m-1,n-1,dp,grid);
+    }
+    private static int fk(int i,int j,int[][] dp,int[][]grid){
+        if(i<0||j<0)  return (int) 1e9;
+        if(i == 0 && j == 0) return grid[0][0];
+        if(dp[i][j]!= -1) return dp[i][j];
+        // Compute path by going up
+        int up = grid[i][j] + fk(i - 1, j,dp,grid);
+        // Compute path by going left
+        int left = grid[i][j] + fk(i, j - 1, dp,grid);
+        return dp[i][j] = Math.min(up,left);
+    } 
 }
